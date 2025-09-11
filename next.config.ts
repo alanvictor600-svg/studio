36baa3957ee9f744cbbbac4d945891e7fffe5710
allan victor <alan.victor600@gmail.com>
2025-09-11 02:00:12 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -20,6 +20,9 @@ const nextConfig: NextConfig = {
       // Removed placehold.co as it's no longer used
     ],
   },
+  experimental: {
+    allowedDevOrigins: ["https://6000-firebase-studio-1747168607847.cluster-m7tpz3bmgjgoqrktlvd4ykrc2m.cloudworkstations.dev"],
+  }
 };
 
 export default nextConfig;
