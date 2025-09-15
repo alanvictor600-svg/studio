9b85b344a50312d8b7a478e532e21ed7043ee73f
allan victor <alan.victor600@gmail.com>
2025-09-15 01:17:19 +0000

    pergunta nao esta precisando ativar nada manualmento no firebase? essse

--- src/app/vendedor/page.tsx
+++ src/app/vendedor/page.tsx
@@ -414,3 +414,4 @@ export default function VendedorPage() {
       </footer>
     </div>
   );
+}
--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,4 +1,3 @@
-
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
@@ -15,7 +14,6 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-// Avoid re-initializing on Next.js hot-reloads
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
 // Initialize Firestore with offline persistence, but only on the client-side.
