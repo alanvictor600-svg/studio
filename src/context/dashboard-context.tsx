d48499b5d31558502c457c679d447fcbe5a25ea5
allan victor <alan.victor600@gmail.com>
2025-09-21 23:48:30 +0000

    Error: Insufficient credits.     at https://6000-firebase-studio-1747168

--- src/context/dashboard-context.tsx
+++ src/context/dashboard-context.tsx
@@ -84,7 +84,7 @@ export const DashboardProvider = ({ children }: { children: ReactNode }) => {
 
         // 2. Draws Listener
         const drawsQuery = query(collection(db, 'draws'));
-        unsubscribes.push(onSnapshot(drawsQuery, (drawsSnapshot) => {
+        unsubscribes.push(onSnapshot(drawsSnapshot, (drawsSnapshot) => {
             const drawsData = drawsSnapshot.docs.map(d => ({ id: d.id, ...d.data() } as Draw));
             setAllDraws(drawsData);
             setIsLotteryPaused(drawsData.length > 0);
